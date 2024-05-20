34c2a86265f2661b219a88015ecf2264668a46e0
Sarah Swiston <sarak.k.swiston@gmail.com>
2024-05-20 14:12:43 -0500

    basic and slim images for arm/amd

---
+++ basic_arm64/test.sh
@@ -0,0 +1,9 @@
+## PROGRAMS FOR TESTING:
+
+# PYTHON
+echo ""; echo "Checking for python..."; echo ""
+python --version
+
+# R
+echo ""; echo "Checking for R..."; echo ""
+R --version | head -n 2 | tail -n 1
